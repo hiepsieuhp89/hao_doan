e701eaaf40c1ae2887eb052bb73efab435a64cf8
xuantung <tungcan2000@gmail.com>
2022-04-03 16:21:42 +0700

    t

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ define('WEB_PUBLIC', 'http://'.$_SERVER['SERVER_NAME'].$host.'/public');
 
 defined('APPLICATION_PATH') ||
         define('APPLICATION_PATH', __DIR__);
-define("URL_UPLOAD", "/public");
+define("URL_UPLOAD", "/media");
 if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
     return false;
 }
